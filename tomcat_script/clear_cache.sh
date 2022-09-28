1fa3fdd54ff888768456a41c29904be820bb34db
mustak09mbstu <37360197+mustak09mbstu@users.noreply.github.com>
2022-09-28 11:17:42 +0600

    Update clear_cache.sh

--- tomcat_script/clear_cache.sh
+++ tomcat_script/clear_cache.sh
@@ -6,7 +6,7 @@
 
 app_server_name="AppServer6"
 tomcat_version="apache-tomcat-9.0.45"
-cd /apps/tomcat/srvvalmetpdm/$app_server_name/$tomcat_version/work/Catalina/
+cd /apps/tomcat/srvtest/$app_server_name/$tomcat_version/work/Catalina/
 rm -rf localhost
 printf "All cache files have been deleted successfully from $app_server_name.\n\n"
 
