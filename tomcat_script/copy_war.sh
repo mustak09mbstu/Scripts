9790d9b8b7dad88c50fb549f79c12c49323d5dcc
mustak09mbstu <37360197+mustak09mbstu@users.noreply.github.com>
2022-09-28 11:13:03 +0600

    Update copy_war.sh
    test

--- tomcat_script/copy_war.sh
+++ tomcat_script/copy_war.sh
@@ -6,8 +6,8 @@
 
 app_server_name="AppServer6"
 tomcat_version="apache-tomcat-9.0.45"
-warlocation="/apps/tomcat/srvvalmetpdm/automation"
-cp -r $warlocation/valmet.war  /apps/tomcat/srvvalmetpdm/$app_server_name/$tomcat_version/webapps
+warlocation="/apps/tomcat/srvtest/automation"
+cp -r $warlocation/test.war  /apps/tomcat/srvtest/$app_server_name/$tomcat_version/webapps
 
 echo "War has been copied successfully"
 
