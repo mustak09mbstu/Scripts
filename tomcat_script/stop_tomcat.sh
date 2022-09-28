4894d8d8567507848005bfc16065006ce97672bd
mustak09mbstu <37360197+mustak09mbstu@users.noreply.github.com>
2022-09-28 11:15:43 +0600

    Update stop_tomcat.sh

--- tomcat_script/stop_tomcat.sh
+++ tomcat_script/stop_tomcat.sh
@@ -1,8 +1,8 @@
 # Stop tomcat server
 
-echo "Change directory to /apps/tomcat/srvvalmetpdm/AppServer6/apache-tomcat-9.0.45/bin"
+echo "Change directory to /apps/tomcat/srvtest/AppServer6/apache-tomcat-9.0.45/bin"
 
-cd "/apps/tomcat/srvvalmetpdm/AppServer6/apache-tomcat-9.0.45/bin/"
+cd "/apps/tomcat/srvtest/AppServer6/apache-tomcat-9.0.45/bin/"
 
 echo "Executing shutdown.sh file......."
 
