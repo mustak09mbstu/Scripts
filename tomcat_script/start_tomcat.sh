f40e8a6af92c77495d7c544f7fdab0f7f860a732
mustak09mbstu <37360197+mustak09mbstu@users.noreply.github.com>
2022-09-28 11:14:51 +0600

    Update start_tomcat.sh
    test

--- tomcat_script/start_tomcat.sh
+++ tomcat_script/start_tomcat.sh
@@ -1,6 +1,6 @@
-echo "Change directory to /apps/tomcat/srvvalmetpdm/AppServer6/apache-tomcat-9.0.45/bin/"
+echo "Change directory to /apps/tomcat/srvtest/AppServer6/apache-tomcat-9.0.45/bin/"
 
-cd "/apps/tomcat/srvvalmetpdm/AppServer6/apache-tomcat-9.0.45/bin/"
+cd "/apps/tomcat/srvtest/AppServer6/apache-tomcat-9.0.45/bin/"
 
 echo "Executing startServer.sh file......."
 
